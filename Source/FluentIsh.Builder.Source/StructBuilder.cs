0c90258ed1b3b6dd834db8817a31d5bbb5615b89
Filip Drabinski <58997858+Filip-Drabinski@users.noreply.github.com>
2022-02-06 22:23:22 +0100

    Remove unnecessary using directives

--- Source/FluentIsh.Builder.Source/StructBuilder.cs
+++ Source/FluentIsh.Builder.Source/StructBuilder.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Runtime.CompilerServices;
+using FluentIsh.Builder.Source.Interfaces.Shared;
 
 [assembly: InternalsVisibleTo("FluentIsh.Builder.Source.Struct.Tests")]
 namespace FluentIsh.Builder.Source
